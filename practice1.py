82de1bd712dda26e949ae0d9d445c4c0fe5574b3
caitig131 <31958666+caitig131@users.noreply.github.com>
2018-05-02 14:36:48 -0400

    new

--- practice1.py
+++ practice1.py
@@ -1,4 +1,5 @@
-from ex1 import test
+from unit_tester import test
+
 
 friends = ["Joe", "Zoe", "Brad", "Angelina", "Zuki", "Thandi", "Paris"]
 
@@ -8,6 +9,21 @@ def search_linear(xs, target):
        if v == target:
            return i
     return -1
+
+def find_unknown_words(vocab, wds):
+    """ Return a list of words in wds that do not occur in vocab """
+    result = []
+    for w in wds:
+        if (search_linear(vocab, w) < 0):
+            result.append(w)
+    return result
+
+vocab = ["apple", "boy", "dog", "down","fell", "girl", "grass", "the", "tree"]
+book_words = "the apple fell from the tree to the grass".split()
+
+test(find_unknown_words(vocab, book_words) == ["from", "to"])
+test(find_unknown_words([], book_words) == book_words)
+test(find_unknown_words(vocab, ["the", "boy", "fell"]) == [])
 
 test(search_linear(friends, "Zoe") == 1)
 test(search_linear(friends, "Joe") == 0)
